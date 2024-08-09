67ff75d4b251e944cd03a595b519c5432def6ea3
galih752 <rd.galih752@gmail.com>
2024-08-09 15:37:38 +0700

    Fix Pusher

--- pusher_nextpage.py
+++ pusher_nextpage.py
@@ -8,42 +8,54 @@ import requests
 import pyssdb
 
 # Initialize Greenstalk and SSDB clients once
-greenstalk_client = greenstalk.Client(('192.168.20.175', 11300), watch='sc_bps_daerah_baselink_new_fix')
+greenstalk_client = greenstalk.Client(
+    ("192.168.20.175", 11300), watch="sc_bps_daerah_baselink_new_fix"
+)
 ssdb_client = pyssdb.Client(
     host="192.168.150.21",
     port=8888,
     max_connections=500,
 )
 
+client_detail = greenstalk.Client(
+    ("192.168.20.175", 11300), use="sc_bps_daerah_detail_new_fix"
+)
 
 cookies = {
-    'f5avraaaaaaaaaaaaaaaa_session_': 'JJIBFCAPHIDKPICCBJHACEGODKPNBHFPMPGNJBFAJPIDGHOAFKILCJKJLIODBGCONFIDDDGKAGBMGBILNNNADHPGCMPFPFJNFDKOEPOOPGBFNPHBELHGEBPPKODMEFEB',
-    '_ga_CLP8Q4CR7J': 'GS1.1.1721987499.2.1.1721988896.0.0.0',
-    '_ga_MBEH1B2Q1F': 'GS1.1.1722074871.2.1.1722074881.0.0.0',
-    '_ga_SLZ6H0R0CX': 'GS1.1.1722191457.5.0.1722191457.60.0.0',
-    '_ga_T7YPSCVK8R': 'GS1.1.1722221423.2.0.1722221423.0.0.0',
-    '_ga_7FD8073S86': 'GS1.1.1722228771.11.1.1722228937.0.0.0',
-    '_ga_578WLYVT58': 'GS1.1.1722236209.6.1.1722236210.0.0.0',
-    '_ga': 'GA1.1.984331042.1721381442',
-    'asw': '{"lang":"id"}',
-    'e437b945144c4368eba347f6b8fbb958': '2446141807173d3ff0517b7bf5f0b03e',
-    'f5avraaaaaaaaaaaaaaaa_session_': 'CPEKBKBAJJDLPCFNOLNJEJEJJBPBJMOAJAPJNMFMAOICALNIJMFNDJOHJFNOGMBIMEEDKPDDBGKOADAHPECAMHNGCMMFBKPEFELCBAOBOHEOCBHNCKPGEONNDLLJINEM',
-    '_ga_XXTTVXWHDB': 'GS1.1.1723188762.70.1.1723189036.0.0.0',
-    'bpscinfo': 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJCYWRhbiBQdXNhdCBTdGF0aXN0aWsiLCJqdGkiOiJueXJaU3djVm9NVUx0NWRyMmZ5TF8iLCJpYXQiOjE3MjMxODg3MzcsImV4cCI6MTcyMzE5NjIzN30.1LKp_w_DU9aGd40ypkubS-bAoF8iig9P9fft6tgBGWE',
-    'TS01395fde': '0167a1c861d50b763a3f97a32caa19c1d993c278471b73dc44ce943f5e8d87f8bb129ba7224b7a70b534ec687f129da158f83333003d8c1cbf092a08cdcc1a028258af497ff50a90732215204f4545ea6deeb38a6f42678aa43f820227527a2797d1f63059',
+    "f5avraaaaaaaaaaaaaaaa_session_": "JJIBFCAPHIDKPICCBJHACEGODKPNBHFPMPGNJBFAJPIDGHOAFKILCJKJLIODBGCONFIDDDGKAGBMGBILNNNADHPGCMPFPFJNFDKOEPOOPGBFNPHBELHGEBPPKODMEFEB",
+    "_ga_CLP8Q4CR7J": "GS1.1.1721987499.2.1.1721988896.0.0.0",
+    "_ga_MBEH1B2Q1F": "GS1.1.1722074871.2.1.1722074881.0.0.0",
+    "_ga_SLZ6H0R0CX": "GS1.1.1722191457.5.0.1722191457.60.0.0",
+    "_ga_T7YPSCVK8R": "GS1.1.1722221423.2.0.1722221423.0.0.0",
+    "_ga_7FD8073S86": "GS1.1.1722228771.11.1.1722228937.0.0.0",
+    "_ga_578WLYVT58": "GS1.1.1722236209.6.1.1722236210.0.0.0",
+    "_ga": "GA1.1.984331042.1721381442",
+    "asw": '{"lang":"id"}',
+    "e437b945144c4368eba347f6b8fbb958": "2446141807173d3ff0517b7bf5f0b03e",
+    "f5avraaaaaaaaaaaaaaaa_session_": "CPEKBKBAJJDLPCFNOLNJEJEJJBPBJMOAJAPJNMFMAOICALNIJMFNDJOHJFNOGMBIMEEDKPDDBGKOADAHPECAMHNGCMMFBKPEFELCBAOBOHEOCBHNCKPGEONNDLLJINEM",
+    "_ga_XXTTVXWHDB": "GS1.1.1723188762.70.1.1723189036.0.0.0",
+    "bpscinfo": "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJCYWRhbiBQdXNhdCBTdGF0aXN0aWsiLCJqdGkiOiJueXJaU3djVm9NVUx0NWRyMmZ5TF8iLCJpYXQiOjE3MjMxODg3MzcsImV4cCI6MTcyMzE5NjIzN30.1LKp_w_DU9aGd40ypkubS-bAoF8iig9P9fft6tgBGWE",
+    "TS01395fde": "0167a1c861d50b763a3f97a32caa19c1d993c278471b73dc44ce943f5e8d87f8bb129ba7224b7a70b534ec687f129da158f83333003d8c1cbf092a08cdcc1a028258af497ff50a90732215204f4545ea6deeb38a6f42678aa43f820227527a2797d1f63059",
 }
+
 
 def proses_job(data):
     params = {
-        "subject": data['subject'],
+        "subject": data["subject"],
     }
-    request_data = [{"locale": "id", "keyword": "$undefined", "subject": data["subject"], "page": data["page"], "perpage": 100, "sortBy": "date", "sortOrder": "desc"}]
+    request_data = [
+        {
+            "locale": "id",
+            "keyword": "$undefined",
+            "subject": data["subject"],
+            "page": data["page"],
+            "perpage": 100,
+            "sortBy": "date",
+            "sortOrder": "desc",
+        }
+    ]
     request_data_json = json.dumps(request_data)
-
-    print(request_data)
-
     beta_url_table = f"{data['link']}/id/statistics-table?subject={params['subject']}"
-    print(beta_url_table)
 
     headers = {
         "accept": "text/x-component",
@@ -62,7 +74,7 @@ def proses_job(data):
         "sec-fetch-site": "same-origin",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
     }
-    
+
     try:
         response = requests.post(
             beta_url_table,
@@ -73,61 +85,71 @@ def proses_job(data):
         )
 
         response_text = response.content.decode("utf-8")
-        text = re.sub('\n','', response_text)
-        text = re.sub('.*]]1:','', text)
-
-        print(response)
-
+        text = re.sub("\n", "", response_text)
+        text = re.sub(".*]]1:", "", text)
         items = json.loads(text)
+        index = 1
 
-        for item in items["response"]["data"][1]:
-            title = item["title"].lower().replace(" ", "-")
-            title = re.sub("[^A-z0-9]", "-", title)
-            link = f"{beta_url_table}/{item['tablesource']}/{item['id']}/{title}.html"
-            
-            metadata = {
-                "link": link,
-                "domain": data['link'].split("/")[2],
-                "tag": ["bps", "bps.go.id", "statistics table"],
-                "category": item["subject"],
-                "sub_category": item["subcat"],
-                "title": item["title"],
-            }
 
-            exist = ssdb_client.hexists("{}".format('sc_bps_daerah_links_new_fix'), "{}".format(item['id']))
-            exist = exist.decode("utf-8")
-            if exist == "0":
-                hset = ssdb_client.hset(
-                        'sc_bps_daerah_links_new_fix', 
-                        item['id'], 
-                        json.dumps(metadata)
-                    )
-                print(f"Successfully added {item['id']} to ssdb")
-                
-                if hset:
-                    client_detail = greenstalk.Client(('192.168.20.175', 11300), use='sc_bps_daerah_detail_new_fix')
-                    client_detail.put(json.dumps(metadata, indent=2), ttr=3600)
-            else:
-                print(f"Already added {item['id']} to ssdb")
-            
-        try:
-            pages = items["response"]["data"][0]["pages"]
 
-            for page in range(2, pages+1):
-                metadata_page = {
+        if "data" in items["response"] and items["response"]["data"] is not None:
+            if not items["response"]["data"]:
+                raise Exception("posts empty")
+            if len(items["response"]["data"]) == 1:
+                index = 0
+            for item in items["response"]["data"][index]:
+                title = item["title"].lower().replace(" ", "-")
+                title = re.sub("[^A-z0-9]", "-", title)
+                link = (
+                    f"{beta_url_table}/{item['tablesource']}/{item['id']}/{title}.html"
+                )
+
+                metadata = {
                     "link": link,
-                    "domain": data['link'].split('/')[2],
-                    "tag": ["bps", data['link'].split('/')[2], "statistics table"],
-                    "page": page,
-                    "subject": data["subject"],
+                    "domain": data["link"].split("/")[2],
+                    "tag": ["bps", "bps.go.id", "statistics table"],
+                    "category": item["subject"],
+                    "sub_category": item["subcat"],
+                    "title": item["title"],
                 }
-                client_nextpage = greenstalk.Client(('192.168.20.175', 11300), use='sc_bps_daerah_list_new')
-                client_nextpage.put(json.dumps(metadata_page, indent=2), ttr=3600)
+                exist = ssdb_client.hexists(
+                    "{}".format("sc_bps_daerah_links_new_fix"), "{}".format("saljdj")
+                )
+                exist = exist.decode("utf-8")
+                if exist == "0":
+                    ssdb_client.hset(
+                        "sc_bps_daerah_links_new_fix", item["id"], json.dumps(metadata)
+                    )
 
-                print("Successfully added {} to sc_bps_daerah_list page : {}".format(data["subject"], page))
-        except Exception as e:
-            print(e)
-            print(traceback.format_exc())
+                    client_detail.put(json.dumps(metadata, indent=2), ttr=3600)
+                    print(f"Successfully added {item['id']} to ssdb & beanstalk")
+                else:
+                    print(f"Already added {item['id']} to ssdb")
+
+            try:
+                pages = items["response"]["data"][0]["pages"]
+
+                for page in range(2, pages + 1):
+                    metadata_page = {
+                        "link": link,
+                        "domain": data["link"].split("/")[2],
+                        "tag": ["bps", data["link"].split("/")[2], "statistics table"],
+                        "page": page,
+                        "subject": data["subject"],
+                    }
+                    client_nextpage = greenstalk.Client(
+                        ("192.168.20.175", 11300), use="sc_bps_daerah_list_new"
+                    )
+                    client_nextpage.put(json.dumps(metadata_page, indent=2), ttr=3600)
+
+                    print(
+                        "Successfully added {} to sc_bps_daerah_list page : {}".format(
+                            data["subject"], page
+                        )
+                    )
+            except Exception as e:
+                print(e)
+                print(traceback.format_exc())
 
         return True
     except Exception as e:
@@ -135,11 +157,12 @@ def proses_job(data):
         print(traceback.format_exc())
         return False
 
+
 def main():
     while True:
         job = None
         try:
-            job = greenstalk_client.reserve(timeout=10)
+            job = greenstalk_client.reserve(1)
             data = json.loads(job.body)
             success = proses_job(data)
 
@@ -160,6 +183,7 @@ def main():
             if job:
                 greenstalk_client.bury(job)
         time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
